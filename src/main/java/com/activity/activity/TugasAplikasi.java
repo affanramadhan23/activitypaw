d167861fe9907a5001c5709fa1edfa43f602eab5
affanramadhan23 <affannabilramadhan@gmail.com>
2023-08-03 21:31:29 +0700

    Menambahkan File file tugas

---
+++ src/main/java/com/activity/activity/TugasAplikasi.java
@@ -0,0 +1,12 @@
+package com.activity.activity;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TugasAplikasi {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TugasAplikasi.class, args);
+	}
+
+}
