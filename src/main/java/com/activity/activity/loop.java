90ca622a0e849a2d6e174910e0900a70aab55941
affanramadhan23 <affannabilramadhan@gmail.com>
2023-08-04 10:21:52 +0700

    loop

---
+++ src/main/java/com/activity/activity/loop.java
@@ -0,0 +1,9 @@
+package com.activity.activity;
+
+public class loop {
+    public static void main(String[] args) {
+        for(;;){
+            System.out.println("Perulangan tanpa henti");
+        }
+    }
+}
