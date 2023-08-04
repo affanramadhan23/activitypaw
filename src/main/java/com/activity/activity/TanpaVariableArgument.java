ce1ac41dd58f028fb5942bcebbc8410b3ed0b8d8
affanramadhan23 <affannabilramadhan@gmail.com>
2023-08-04 11:07:47 +0700

    Tanpa Variable Argument

---
+++ src/main/java/com/activity/activity/TanpaVariableArgument.java
@@ -0,0 +1,17 @@
+package com.activity.activity;
+
+public class TanpaVariableArgument {
+    static void sayCongrats(String name, int[] values) {
+        int total = 0;
+        for (var value : values) {
+            total += value;
+        }
+        int finalvalue = total / values.length;
+
+        if (finalvalue >= 75) {
+            System.out.println("Selamat " + name + ",Anda Lulus");
+        } else {
+            System.out.println("Maaf" + name + ", Anda Lulus");
+        }
+    }
+}
