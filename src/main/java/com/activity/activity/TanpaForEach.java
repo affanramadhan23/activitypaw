d8b40ed4942d8fc13f604a2798bc0f2958052941
affanramadhan23 <affannabilramadhan@gmail.com>
2023-08-04 10:33:09 +0700

    TanpaForEach

---
+++ src/main/java/com/activity/activity/TanpaForEach.java
@@ -0,0 +1,13 @@
+package com.activity.activity;
+
+public class TanpaForEach {
+    public static void main(String[] args) {
+        String[] array = {
+            "Affan", "Nabil", "Ramadhan",
+            "Programmer", "Pintar", "Joss"
+        };
+        for (int i = 0; i < array.length; i++) {
+            System.out.println(array[i]);
+        }
+    }
+}
