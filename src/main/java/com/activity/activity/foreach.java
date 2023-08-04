9cbe35442f49ca301987bf2e2f8d4d94cb57de7c
affanramadhan23 <affannabilramadhan@gmail.com>
2023-08-04 10:38:49 +0700

    foreach

---
+++ src/main/java/com/activity/activity/foreach.java
@@ -0,0 +1,14 @@
+package com.activity.activity;
+
+public class foreach {
+    public static void main(String[] args) {
+        String[] array = {
+            "Affan", "Nabil", "Ramadhan",
+            "Gini", "Amat", "Mancing"
+        };
+
+        for (var value : array) {
+            System.out.println(value);
+        }
+    }
+}
