d60d846614f2ac045a83b1a5c93e7e615d887d85
affanramadhan23 <affannabilramadhan@gmail.com>
2023-08-04 10:23:49 +0700

    loop dengan init statement

---
+++ src/main/java/com/activity/activity/forloop_denganninitstatement.java
@@ -0,0 +1,10 @@
+package com.activity.activity;
+
+public class forloop_denganninitstatement {
+    public static void main(String[] args) {
+        for (var counter = 1; counter <=10; ) {
+            System.out.println("Perulangan ke-" + counter);
+            counter++;
+        }
+    }
+}
