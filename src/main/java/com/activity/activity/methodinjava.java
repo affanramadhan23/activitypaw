f75a9a214f89d982c0daae8a69e3dfce4f3225f3
affanramadhan23 <affannabilramadhan@gmail.com>
2023-08-04 10:46:18 +0700

    add method in java

---
+++ src/main/java/com/activity/activity/methodinjava.java
@@ -0,0 +1,12 @@
+package com.activity.activity;
+
+public class methodinjava {
+    public static void main(String[] args) {
+        sayHelloWorld();
+    }
+
+static void sayHelloWorld(){
+    System.out.println("Hello World");
+}
+
+}
