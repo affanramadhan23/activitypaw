884c1835455c8c45c183bd46bf8ff0b04d99a510
affanramadhan23 <affannabilramadhan@gmail.com>
2023-08-04 10:22:28 +0700

    loop dengan kondisi

---
+++ src/main/java/com/activity/activity/loop_kondisi.java
@@ -0,0 +1,12 @@
+package com.activity.activity;
+
+public class loop_kondisi {
+    public static void main(String[] args) {
+        var counter = 1;
+
+        for (; counter <= 10; ) {
+            System.out.println("Perulangan ke-" + counter);
+            counter++;
+        }
+    }
+}
