cc3abc34fc8b6188898619dd55a128e847e10a33
affanramadhan23 <affannabilramadhan@gmail.com>
2023-08-05 10:03:00 +0700

    add file variable argument, factorial loop, method overloading

---
+++ src/main/java/com/activity/activity/DenganVariableArgument.java
@@ -0,0 +1,18 @@
+//package com.activity.activity;
+
+//public class DenganVariableArgument {
+//    public static void main(String[] args) {
+//        sayCongrats(name: "Affan", ...values: 80, 90, 79, 50);
+//    }
+//
+//    static void sayCongrats(String name, int... values) {
+//        int total = 0;
+//        for (var value : values) {
+//            total += values;
+//        }
+//        int finalvalue = total / values.length;
+//        if (finalvalue >= 75) {
+//            System.Out.println("Selamat " + name + ", Anda Lulus");
+//        } else {}
+//    }
+//}
