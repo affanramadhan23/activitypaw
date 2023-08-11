59abcbcbde7592828cd14560a16b496dd7f0c5c6
affanramadhan23 <affannabilramadhan@gmail.com>
2023-08-11 11:23:46 +0700

    add person add app persos

---
+++ src/main/java/com/activity/activity/BelajarOOP/Person.java
@@ -0,0 +1,17 @@
+package com.activity.activity.BelajarOOP;
+
+public class Person {
+    String name;
+    String address;
+    final String country = "Indonesia";
+
+    //Person(String paramName, String paramAddress) {
+        //name = paramName;
+        //address = paramAddress;
+
+    //}
+
+    void sayHello(String paramName) {
+        System.out.println("Hello " + paramName + ", My Name Is " + name);
+    }
+}
